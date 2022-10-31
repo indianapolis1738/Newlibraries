c9a3b8948d474295f510a3ddc4ddb4437addcc1a
mistercarp <77747892+mistercarp@users.noreply.github.com>
2022-10-31 20:57:34 +0100

    libraries update

---
+++ play/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.carp.app',
+  appName: 'play',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
